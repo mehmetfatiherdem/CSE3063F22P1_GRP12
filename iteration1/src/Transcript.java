cd498ca1ffbc77c2ba31a54960f32203cf979cc6
sadık akgedik <s_akgedik4242@windowslive.com>
2022-11-10 19:38:46 +0300

    Transcript and CourseRecord classes' details

--- iteration1/src/Transcript.java
+++ iteration1/src/Transcript.java
@@ -1,5 +1,40 @@
 package iteration1.src;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import iteration1.src.course.Course;
+import iteration1.src.course.CourseRecord;
+
 public class Transcript {
     private float GPA;
+    private List<CourseRecord> takenCourseRecords = new ArrayList<CourseRecord>();
+    private enum Status{
+        FAILED,PASSED
+    }
+
+    //getters
+    public float getGPA() {
+        return GPA;
+    }
+    public List<CourseRecord> getTakenCourseRecords() {
+        return takenCourseRecords;
+    }
+
+    //setters
+    public void setGPA(float GPA) {
+        this.GPA = GPA;
+    }
+    public void setTakenCourseRecords(List<CourseRecord> takenCourseRecords) {
+        this.takenCourseRecords = takenCourseRecords;
+    }
+
+    public String setCourseStatus(Course course,Status status){
+        
+        return "Not implemented";
+    }
+    public int getCompletedCredits(){
+        // Not implemented yet.
+        return 0;
+    }
 }
--- iteration1/src/course/CourseRecord.java
+++ iteration1/src/course/CourseRecord.java
@@ -1,4 +1,65 @@
 package iteration1.src.course;
 
 public class CourseRecord {
+    //DataFields
+    private enum LetterGrade{
+        AA,BA,BB,CB,CC,DC,DD,FD,FF,ZZ
+    }
+    private enum Season{
+        FALL,SPRING,SUMMER
+    }
+    private Course course;
+    private LetterGrade lGrade;
+    private Season season;
+    private float grade;
+    private int year;
+    private Boolean isPassed;
+
+    //Getters
+    public Course getCourse() {
+        return course;
+    }
+    public float getGrade() {
+        return grade;
+    }
+    public int getYear() {
+        return year;
+    }
+    public Boolean getIsPassed() {
+        return isPassed;
+    }
+    public Season getSeason() {
+        return season;
+    }
+    public LetterGrade getlGrade() {
+        return lGrade;
+    }
+    
+
+    //Setters
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setIsPassed(Boolean isPassed) {
+        this.isPassed = isPassed;
+    }
+    public void setlGrade(LetterGrade lGrade) {
+        this.lGrade = lGrade;
+    }
+    public void setSeason(Season season) {
+        this.season = season;
+    }
+
+    //Get season details Method
+    public void getFullSeasonDetails(){
+
+    }
+    
+
 }
