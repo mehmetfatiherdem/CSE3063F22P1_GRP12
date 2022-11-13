31d9d0d1ba2b2f56195aef6e35dd6205eb75218c
Mehmetakguunn <94480457+Mehmetakguunn@users.noreply.github.com>
2022-11-13 22:09:17 +0300

    Update iteration1/src/human/Lecturer.java
    Co-authored-by: Mustafa Tunahan BAŞ <m.tunahanbas90@gmail.com>

--- iteration1/src/human/Lecturer.java
+++ iteration1/src/human/Lecturer.java
@@ -2,7 +2,7 @@ package iteration1.src.human;
 
 public class Lecturer extends FacultyMember{
 
-    protected Lecturer(String firstName, String lastName) {
+    public Lecturer(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
