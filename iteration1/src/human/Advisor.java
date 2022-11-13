4f4b0c385424bb58cc9f658a963e58fd2ba951c8
Mehmetakguunn <94480457+Mehmetakguunn@users.noreply.github.com>
2022-11-13 22:09:46 +0300

    Update iteration1/src/human/Advisor.java
    Co-authored-by: Mustafa Tunahan BAŞ <m.tunahanbas90@gmail.com>

--- iteration1/src/human/Advisor.java
+++ iteration1/src/human/Advisor.java
@@ -2,9 +2,8 @@ package iteration1.src.human;
 
 public class Advisor extends Lecturer {
 
-    protected Advisor(String firstName, String lastName) {
+    public Advisor(String firstName, String lastName) {
         super(firstName, lastName);
-        
     }
 
     public Advisor(String firstName, String middleName, String lastName) {
