4c2a4b4f0d19f64d4298202b0b214a727c70a45d
Berat Demirhan <beratdemrhan@gmail.com>
2022-11-13 21:30:22 +0300

    HumanTest added

--- iteration1/src/Test.java
+++ iteration1/src/Test.java
@@ -1,7 +1,22 @@
 package iteration1.src;
+import iteration1.src.human.Assistant;
+import iteration1.src.human.Lecturer;
+import iteration1.src.human.Student;
+import iteration1.src.human.Human;
+
 
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is testing");
+        humanTest();
+    }
+    public static void humanTest(){
+        System.out.println("Trying to call the constructor without middle name");
+        System.out.println("");
+        Human student = new Student("Ahmet","Şahin");
+        System.out.println("Trying to call full name");
+        System.out.println(student.getFullName());
+
     }
 }
+
