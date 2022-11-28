4b93c6bd9e5f253b41c981ac7b3a313fc7811c6f
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-11-28 22:07:04 +0300

    addi junit setup

---
+++ iteration1/src/Calculator.java
@@ -0,0 +1,20 @@
+package iteration1.src;
+
+import java.util.stream.DoubleStream;
+
+//TODO: remove this
+public class Calculator {
+
+    public static double add(double... operands) {
+        return DoubleStream.of(operands)
+                .sum();
+    }
+
+    public static double multiply(double... operands) {
+        return DoubleStream.of(operands)
+                .reduce(1, (a, b) -> a * b);
+    }
+
+}
+
+
---
+++ iteration1/test/iteration1/src/CalculatorTest.java
@@ -0,0 +1,23 @@
+package iteration1.src;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Add two numbers")
+    void add() {
+        assertEquals(4, Calculator.add(2, 2));
+    }
+    @Test
+    @DisplayName("Multiply two numbers")
+    void multiply() {
+        assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
+                () -> assertEquals(-4, Calculator.multiply(2, -2)),
+                () -> assertEquals(4, Calculator.multiply(-2, -2)),
+                () -> assertEquals(0, Calculator.multiply(1, 0)));
+    }
+}
