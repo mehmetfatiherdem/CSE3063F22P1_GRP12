3d02f50d7bd435de7a8383d206910be23e0b6a88
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-12-26 21:53:32 +0300

    add method signatures for human

--- iteration3/Human.py
+++ iteration3/Human.py
@@ -1,1 +1,15 @@
-#this is human
+class Human:
+    def __init__(self):
+        pass
+    
+    def getFullName():
+        pass
+
+    def getFirstName():
+        pass
+
+    def getMiddleName():
+        pass
+
+    def getLastName():
+        pass
