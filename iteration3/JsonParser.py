6517eb87410e2d901298af6cd95b6f92cc634e90
XeniaPhe <m.tunahanbas90@gmail.com>
2022-12-26 23:29:07 +0300

    JsonParser and HorizontalLineType added

---
+++ iteration3/JsonParser.py
@@ -0,0 +1,68 @@
+class JsonParser:
+    advisorsFile = "iteration2/resources/Advisors.json"
+    assistantsFile = "iteration2/resources/Assistants.json"
+    lecturersFile = "iteration2/resources/Lecturers.json"
+    coursesFile = "iteration2/resources/Courses.json"
+    semesterFile = "iteration2/resources/Semester.json"
+    studentsDir = "iteration2/resources/students/"
+    studentConfigFile = "iteration2/resources/StudentConfig.json"
+    
+    def __init__(self):
+        pass
+    
+    def parseSemester():
+        pass
+    
+    def parseAdvisors():
+        pass
+    
+    def parseAssistants():
+        pass
+    
+    def parseLecturers():
+        pass
+    
+    def parseCourses(lecturers, assistants):
+        pass
+    
+    def parseStudents(advisors, courses):
+        pass
+    
+    def serializeStudents(students):
+        pass
+    
+    def parseStudentConfig():
+        pass
+    
+    def parseHuman(obj):
+        pass
+    
+    def parseCourseRecords(recordsJson, courses):
+        pass
+    
+    def parseNames(namesJson):
+        pass
+    
+    def findCourseWithCode(courses, courseCode):
+        pass
+    
+    def findAssistantsFrom(assistantsJson, assistants):
+        pass
+    
+    def findLecturersFrom(lecturersJson, lecturers):
+        pass
+    
+    def findAdvisorByName(advisors, advisorName):
+        pass
+    
+    def assignPrerequisitesToCourses(courses, prerequisiteCodes):
+        pass
+    
+    def readJsonFile(fileName):
+        pass
+    
+    def readJsonFile(file):
+        pass
+    
+    def writeToFile(str, fileName):
+        pass
