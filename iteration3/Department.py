ee9839b3dbb9dfbd4b27abc0e0dcd12d3121ef75
XeniaPhe <m.tunahanbas90@gmail.com>
2022-12-26 22:30:08 +0300

    Add department setup

---
+++ iteration3/Department.py
@@ -0,0 +1,100 @@
+class Department:
+    instance
+    #departmentCode
+    #departmentName
+    #currentSeason
+    #mandatoryCourses
+    #technicalElectiveCourses
+    #facultyTechnicalElectiveCourses
+    #nonTechnicalElectiveCourses
+    #students
+    #lecturers
+    #assistants
+    #advisors
+    #initialized
+    
+    
+    def __init__ (self):
+        self.departmentHead = 'lol'
+        pass
+    
+    def initialize(currentSeason, courses, lecturers, assistants, advisors, students):
+        pass
+    
+    def addNewCourseSection(mandatoryCourse):
+        pass
+    
+    def addNewLabSection(course):
+        pass
+    
+    def getDepartmentCode():
+        pass
+    
+    def getDepartmentName():
+        pass
+    
+    def getCurrentSeason():
+        pass
+    
+    def getStudents():
+        pass
+    
+    def getLecturers():
+        pass
+    
+    def getAssistants():
+        pass
+    
+    def getAdvisors():
+        pass
+    
+    def getAllCourses():
+        pass
+    
+    def getMandatoryCourses():
+        pass
+    
+    def getTechnicalElectiveCourses():
+        pass
+    
+    def getFacultyTechnicalElectiveCourses():
+        pass
+    
+    def getNonTechnicalElectiveCourses():
+        pass
+    
+    def getNewSectionSchedule(course, classHours):
+        pass
+    
+    def generateWeeklyScheduleForAllCourses():
+        pass
+    
+    def generateWeeklyScheduleForMandatoryCourses():
+        pass
+    
+    def generateWeeklyScheduleForElectiveCourses():
+        pass
+    
+    def generateCollisionlessWeeklyScheduleForCourses(courses):
+        pass
+    
+    def getDivisions(courses):
+        pass
+    
+    def getDivision(noOfHours):
+        pass
+    
+    def validateDivision(division, noOfHours):
+        pass
+    
+    def assignScheduleToSection(availableClassHours, division):
+        pass
+    
+    def getAvailableDays(schedule, count):
+        pass
+    
+    def getAvailablePositionsOnDay(schedule, dayIndex, count):
+        pass
+    
+    def getScheduleAtPosition(position, count):
+        pass
