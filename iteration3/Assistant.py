2eb6edb160885b133afd669c9381f0a2cbac9486
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-01-01 17:35:55 +0300

    add assistant

--- iteration3/Assistant.py
+++ iteration3/Assistant.py
@@ -1,6 +1,7 @@
-from mimetypes import init
+from iteration3.FacultyMember import FacultyMember
+class Assistant(FacultyMember):
+    def __init__(self, first_name: str, last_name: str):
+        super().__init__(first_name, last_name)
 
-
-class Assistant:
-    def __init__(self):
-        pass
+    def __init__(self, first_name: str, middle_name: str, last_name: str):
+        super().__init__(first_name, middle_name, last_name)
