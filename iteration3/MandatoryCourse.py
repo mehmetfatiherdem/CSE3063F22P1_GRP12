86aa56aea975e310598101e8ce623f363a936090
Talha SEZER <talhasezer4@gmail.com>
2023-01-02 01:04:57 +0300

    MandatoryCourse class added

--- iteration3/MandatoryCourse.py
+++ iteration3/MandatoryCourse.py
@@ -1,14 +1,29 @@
 import Course
+from typing import List
+import Department
+from Human import Assistant
+from Human import Grade
+from Human import Lecturer
+from Human import Student
+
 
 class MandatoryCourse(Course):
-    def __init__(self, code, name, credits, theoreticalHours, appliedHours, firstYearToTake, firstSeasonToTake, lecturers, assistants):
-        pass
+    def init(self, code, name, credits, theoretical_hours, applied_hours, first_year_to_take,
+                 first_season_to_take, lecturers, assistants):
+        super().init(code, name, credits, theoretical_hours, applied_hours, first_year_to_take,
+                         first_season_to_take, lecturers, assistants)
 
-    def getAvailableLabSections():
-        pass
-    
-    def request_new_course_section():
-        pass
-    
-    def get_course_priority():
-        pass
+    def get_available_lab_sections(self):
+        sections = super().get_available_lab_sections()
+
+        if not sections and self.applied_hours > 0:
+            Department.get_instance().add_new_lab_section(self)
+            sections = super().get_available_lab_sections()
+
+        return sections
+
+    def request_new_course_section(self):
+        Department.getInstance().addNewCourseSection(self)
+
+    def get_course_priority(self):
+        return 3
