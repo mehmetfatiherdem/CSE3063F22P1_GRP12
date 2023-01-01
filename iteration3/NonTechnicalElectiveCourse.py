aac5de3eebdf40c9df33c2017f86e60b84c32383
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-01-01 23:03:42 +0300

    switch snake_case

--- iteration3/NonTechnicalElectiveCourse.py
+++ iteration3/NonTechnicalElectiveCourse.py
@@ -10,21 +10,21 @@ class NonTechnicalElectiveCourse(ElectiveCourse):
         7: 1
     }
 
-    def __init__(self, code, name, credits, theoreticalHours, appliedHours, firstYearToTake, firstSeasonToTake, lecturers, assistants):
-       super().__init__(code, name, credits, theoreticalHours, appliedHours, firstYearToTake, firstSeasonToTake, lecturers, assistants)
+    def __init__(self, code, name, credits, theoretical_hours, applied_hours, first_year_to_take, first_season_to_take, lecturers, assistants):
+       super().__init__(code, name, credits, theoretical_hours, applied_hours, first_year_to_take, first_season_to_take, lecturers, assistants)
     
-    def canStudentTakeCourse(self, student):
+    def can_student_take_course(self, student):
         semester = student.get_student_semester()
         no_of_courses_till_semester = self.get_total_number_of_courses_until_semester(semester)
         return student.get_transcript().get_number_of_nte_courses_passed() < no_of_courses_till_semester \
             and not student.did_student_pass(self)
     
     @staticmethod
-    def getTotalNumberOfCoursesUntilSemester(semester):
+    def get_total_number_of_courses_until_semester(semester):
         no_of_courses_till_semester = 0
         for i in range(semester + 1):
             no_of_courses_till_semester += NonTechnicalElectiveCourse.number_of_courses_takeable_by_semester[i]
         return no_of_courses_till_semester
     
-    def getCoursePriority():
+    def get_course_priority():
         return 0
