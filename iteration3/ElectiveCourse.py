f3c53220874be07f6f53be10da136bebf9b7c2ff
Talha SEZER <talhasezer4@gmail.com>
2023-01-08 01:49:28 +0300

    Update ElectiveCourse

--- iteration3/ElectiveCourse.py
+++ iteration3/ElectiveCourse.py
@@ -1,13 +1,13 @@
 import abc
 from typing import List
 
-from Course import Course
-from Human import Assistant
-from Human import Grade
-from Human import Lecturer
-from Human import Student
+from iteration3.Course import Course
+from iteration3.Assistant import Assistant
+from iteration3.Grade import Grade
+from iteration3.Lecturer import Lecturer
+from iteration3.Student import Student
 
-class ElectiveCourse(Course, metaclass=abc.ABCMeta):
+class ElectiveCourse(Course):
     def __init__(self, code, name, credits, theoretical_hours, applied_hours, first_year_to_take, first_season_to_take, lecturers, assistants):
         super().__init__(code, name, credits, theoretical_hours, applied_hours, first_year_to_take, first_season_to_take, lecturers, assistants)
 
