bf43c548f48c869571a659dabab39dd7995ee39f
Mehmet Akgun <mehmetakgun17062001@gmail.com>
2023-01-08 17:26:17 +0300

    Filled empty methods

--- iteration3/TechnicalElectiveCourse.py
+++ iteration3/TechnicalElectiveCourse.py
@@ -12,23 +12,29 @@ class TechnicalElectiveCourse(ElectiveCourse):
         7: 3
     }
 
-    def __init__(self, code, name, credits, theoreticalHours, appliedHours, firstYearToTake, firstSeasonToTake, lecturers, assistants):
-        pass
-    
-    
-    def addPrerequisite(prerequisite):
-        pass
-    
-    
-    def canStudentTakeCourse(student):
-        pass
-    
-    def isCreditsRequirementMet(student):
-        pass
-    
+    def __init__(self, code, name, credits, theoretical_hours, applied_hours, first_year_to_take, first_season_to_take, lecturers, assistants):
+        super().__init__(code, name, credits, theoretical_hours, applied_hours, first_year_to_take, first_season_to_take, lecturers, assistants)
+
+    def add_prerequisite(self, prerequisite):
+        self.prerequisites.add(prerequisite)
+
+    def can_student_take_course(self, student):
+        semester = student.get_student_semester()
+        no_of_courses_till_semester = self.get_total_number_of_courses_until_semester(semester)
+
+        return student.get_transcript().get_number_of_te_courses_passed() < no_of_courses_till_semester and not student.did_student_pass(self) and self.is_credits_requirement_met(student)
+
+    def is_credits_requirement_met(self, student):
+        return self.REQUIRED_CREDITS <= student.get_completed_credits()
+
     @staticmethod
-    def getTotalNumberOfCoursesUntilSemester(semester):
-        pass
-    
-    def getCoursePriority():
-        pass
+    def get_total_number_of_courses_until_semester(semester):
+        no_of_courses_till_semester = 0
+
+        for i in range(semester + 1):
+            no_of_courses_till_semester += TechnicalElectiveCourse.numberOfCoursesTakeableBySemester[i]
+
+        return no_of_courses_till_semester
+
+    def get_course_priority(self):
+        return 2
