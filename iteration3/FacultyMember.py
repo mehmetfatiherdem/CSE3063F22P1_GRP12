a3ce9d74f9bf529bf1603fe1d5c26f43b5ec530e
XeniaPhe <m.tunahanbas90@gmail.com>
2023-01-08 15:35:09 +0300

    FacultyMember.java is ported to python

--- iteration3/FacultyMember.py
+++ iteration3/FacultyMember.py
@@ -1,3 +1,6 @@
-class FacultyMember:
-    def __init__(self):
-        pass
+from iteration3.src import Department
+
+class FacultyMember(Human):
+    def __init__(self, first_name, middle_name=None, last_name=None):
+        super().__init__(first_name, middle_name, last_name)
+        self.department = Department.get_instance()
