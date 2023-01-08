476c35cc9e0fa5d85d81915030e211cab7e3cd9f
XeniaPhe <m.tunahanbas90@gmail.com>
2023-01-08 15:31:06 +0300

    Human.java is ported to python

--- iteration3/Human.py
+++ iteration3/Human.py
@@ -1,15 +1,17 @@
 class Human:
-    def __init__(self):
-        pass
-    
-    def getFullName():
-        pass
+    def __init__(self, first_name, middle_name=None, last_name=None):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
 
-    def getFirstName():
-        pass
+    def get_full_name(self):
+        return self.first_name + ('' if self.middle_name is None or len(self.middle_name) <= 0 else ' ' + self.middle_name) + ' ' + self.last_name
 
-    def getMiddleName():
-        pass
+    def get_first_name(self):
+        return self.first_name
 
-    def getLastName():
-        pass
+    def get_middle_name(self):
+        return self.middle_name
+
+    def get_last_name(self):
+        return self.last_name
